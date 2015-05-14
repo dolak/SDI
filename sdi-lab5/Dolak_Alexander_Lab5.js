d38bac49db49278ab99f7d1a30e852ca4af89ad4
Alexander <abdolak@fullsail.edu>
2015-05-14 14:41:15 -0400

    dayFiveLab
    This is the calculator final product and stuff

---
+++ sdi-lab5/Dolak_Alexander_Lab5.js
@@ -0,0 +1,71 @@
+/*
+Alexander Dolak
+Lab 5
+5/14/15
+ */
+
+//These are asking the user first what they want to do. Then what 2 numbers they would like to use.
+var choice = prompt("Would you like addition, subtraction, multiplication, or division?", "+, -, *, /");;
+var numOne = parseFloat(prompt("Please enter a number", "Enter your first number here:"));
+var numTwo = parseFloat(prompt("Please enter a number", "Enter your second number here:"));;
+
+//This function is using the user's numbers and adding them together. Then returns the solution.
+function addition(){
+    var solution;
+
+    solution = (numOne + numTwo);
+
+    return(solution);
+}
+
+//This function is using the user's numbers and subtracting them from each other. Then returns the solution.
+function subtraction(){
+    var solution;
+
+    solution = (numOne - numTwo);
+
+    return(solution);
+}
+
+//This function is using the user's numbers and multiplying them to each other. Then returns the solution.
+function multiplication(){
+    var solution;
+
+    solution = (numOne * numTwo);
+
+    return(solution);
+}
+
+//This function is using the user's numbers and dividing them. Then returns the solution.
+function division(){
+    var solution;
+
+    solution = (numOne / numTwo);
+
+    return(solution);
+}
+
+var add     = addition();
+var sub     = subtraction();
+var multi   = multiplication();
+var divi    = division();
+//This if statement takes what function the user wanted, runs it and prints out the results
+if(choice === "+"){
+    addition();
+    console.log(add);
+
+} else if(choice === "-") {
+    subtraction();
+    console.log(sub);
+
+} else if(choice === "*"){
+    multiplication();
+    console.log(multi);
+
+} else if(choice === "/") {
+    division();
+    console.log(divi);
+
+} else {//this is if they decide to mess with the choice and ask for something the program does not support
+    console.log("I am sorry I don't understand. Please re-run the program to try again.");
+}
