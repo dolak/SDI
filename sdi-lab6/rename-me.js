b3ffa8adcfa13003a483fdbb7df60fd9994f907a
Alexander <abdolak@fullsail.edu>
2015-05-16 15:06:19 -0400

    daySixLab
    arrays lab update

--- sdi-lab6/rename-me.js
+++ sdi-lab6/rename-me.js
@@ -1,4 +1,4 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 
 //global
 
@@ -6,19 +6,48 @@ alert("JavaScript works!");
 //function
 function smashBrothers(){
     //arrays
-    var myCharacters = ["Marth", "Captain Falcon", "Pikachu", "Link", "Dr. Mario"];
-    var characterGames = ["Fire Emblem", "F-Zero", "Pokemon", "Legend of Zelda", "Mario"];
+    var myCharacters = ["Captain Falcon", "Pikachu", "Link", "Dr. Mario", "Pikachu"];
+    var characterGames = ["F-Zero", "Pokemon", "Legend of Zelda", "Mario", "Pokemon"];
+    var noMore;
+    var main;
 
-    //for loop
+    console.log("These are the characters I play competitively in Super Smash Bros. Melee and the games they are from.")
+
+    //Here I am stating what characters I play and their games or origin
     for(var i = 0; i < myCharacters.length; i++){
+        if (myCharacters[i] === "Pikachu") {
+            myCharacters[1] = "Jigglypuff"  // Here I am changing one of the characters to another.
+            console.log(myCharacters[i] + " is from the game " + characterGames[i]);
+        } else {
+            console.log(myCharacters[i] + " is from the game " + characterGames[i]);
+        }
+    }
+    console.log("They are oredered by how well I play them.")
+    console.log("")
+
+
+    noMore = myCharacters.pop();        //Here I am removing a character that I no longer play.
+    for(var i = 0; i < myCharacters.length; i++) {
         console.log(myCharacters[i] + " is from the game " + characterGames[i]);
     }
-if(myCharacters[i] === "Pikachu"){
-    myCharacters["2"] = "Jigglypuff"
-    console.log(myCharacters)
-} else{
-    console.log(myCharacters)
-}
+    console.log("I took " + noMore + " out because I do not play them anymore.");
+
+    console.log("")
+    main = myCharacters[0];
+    console.log("The main character I play is " + main);//Here I am stating what the #1 position in the list is
+    console.log("")
+
+    myCharacters.unshift("Marth");          //Here I am putting in another character and the game they are from.
+    characterGames.unshift("Fire Emblem");
+
+    console.log("")
+    console.log("I forgot about another character. Here is the updated list.")  //I am showing the user the updated list.
+    for(var i = 0; i < myCharacters.length; i++) {
+        console.log(myCharacters[i] + " is from the game " + characterGames[i]);
+    }
+    console.log("")
+    main = myCharacters[0];
+    console.log("The main character I play is " + main); //This is the new first place character.
 
 }
 //function call
