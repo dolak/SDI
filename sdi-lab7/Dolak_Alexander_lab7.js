f469279b4ef11fa54e78245d3ca6941e72fabb9a
Alexander <abdolak@fullsail.edu>
2015-05-19 14:22:05 -0400

    daySeven
    fixed my problem but now i have to rewrite the code to use a function.

--- sdi-lab7/Dolak_Alexander_lab7.js
+++ sdi-lab7/Dolak_Alexander_lab7.js
@@ -3,9 +3,12 @@
 Set1 = [10, 6, 7, 3, 12, 5]
 Set2 = [25, 100, 4, 22, 16, 105, 2, 7]
 Set3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42]
-var dump;
-var user = prompt("Type in 'set1', 'set2', or 'set3' to sort the set.");
 
+var dump;
+
+//This is to choose the set to sort
+var user = prompt("Type in 'set1', 'set2', or 'set3' to sort the set.");
+//This is to get the set the user verified.
 if(user === "set1"){
     user = Set1
 } else if (user === "set2"){
@@ -15,16 +18,27 @@ if(user === "set1"){
 } else{
     console.log("I am sorry I do not understand.")
 }
+//This is so i will increment
+    for (var i = 0; i < user.length; i++) {
+//This is so k will increment
+        for (var k = 1; k < user.length; k++) {
+//This is to swap the numbers so they will be in order
+            if(i >= k){
+                k=i+1;
+            }
 
-for(var i = 0; i < user.length; i ++){
+            if (user[i] > user[k]) {
+                dump = user[k];
+                user[k] = user[i];
+                user[i] = dump;
+                //This is so I can see what is going on with my code.
+                console.log("i " + i)
+                console.log("k " + k)
+                console.log("dump " + dump)
 
-    for(var k = 0; k < user.length; k++){
-
-        if(user[i] > user[k + 1]){
-            dump = user[k+1];
-            user[k+1] = user[i];
-            user[i] = dump;
+                console.log(user)
+            }
         }
     }
-}
-console.log(user);
+
+//console.log(user);
