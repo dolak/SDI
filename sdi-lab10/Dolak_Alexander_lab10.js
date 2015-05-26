84efa14f66f15f4ee4df8f032c7ab643aed80ae4
Alexander <abdolak@fullsail.edu>
2015-05-26 13:40:00 -0400

    dayTenLab
    commit 1: have rand working the way I want it to.

--- sdi-lab10/Dolak_Alexander_lab10.js
+++ sdi-lab10/Dolak_Alexander_lab10.js
@@ -1,2 +1,36 @@
 //alert("JavaScript works!");
 
+var returnedValue;
+var userPick = prompt("Are you going to choose the Powerball or the Florida lottery?, Powerball or Florida lottery");
+var numUsed;
+var limitNum
+
+while(userPick.toLowerCase() != "powerball" && userPick.toLowerCase() != "florida lottery"){
+
+    userPick = prompt("Are you going to choose the Powerball or the Florida lottery?, Powerball or Florida lottery");
+
+}
+if (userPick.toLowerCase() === "powerball"){
+
+    numUsed = [5];
+    limitNum = 59;
+
+} else {
+
+    numUsed = [6];
+    limitNum = 35;
+
+}
+
+
+function lottoNum(nums, limNum){
+
+    var rand = parseInt(Math.random() * limNum + 1);
+
+
+
+}
+
+returnedValue = lottoNum(numUsed, limitNum);
+
+console.log(returnedValue)
